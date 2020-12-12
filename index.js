ed6e9eff8f0cbb7024d4bd8c242bb53ecff072d2
testingBOT9 <75772930+testingBOT9@users.noreply.github.com>
2020-12-12 07:27:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -34,6 +34,59 @@ const aris3 = require("./lib/aris3.js");
 const aris4 = require("./lib/aris4.js");
 const readTextInImage = require('./lib/ocr')
 const { create, Client } = require('@open-wa/wa-automate')
+const { create, Client } = require('@open-wa/wa-automate')
+const welcome = require('./lib/welcome')
+const msgHandler = require('./msgHndlr')
+const options = require('./options')
+
+const start = async (client = new Client()) => {
+        console.log('[SERVER] Server Started!')
+        // Force it to keep the current session
+        client.onStateChanged((state) => {
+            console.log('[Client State]', state)
+            if (state === 'CONFLICT' || state === 'UNLAUNCHED') client.forceRefocus()
+        })
+        // listening on message
+        client.onMessage((async (message) => {
+            client.getAmountOfLoadedMessages()
+            .then((msg) => {
+                if (msg >= 3000) {
+                    client.cutMsgCache()
+                }
+            })
+            msgHandler(client, message)
+        }))
+
+        client.onGlobalParicipantsChanged((async (heuh) => {
+            await welcome(client, heuh)
+            //left(client, heuh)
+            }))
+        
+        client.onAddedToGroup(((chat) => {
+            let totalMem = chat.groupMetadata.participants.length
+            if (totalMem < 30) { 
+            	client.sendText(chat.id, `Cih member nya cuma ${totalMem}, Kalo mau invite bot, minimal jumlah mem ada 30`).then(() => client.leaveGroup(chat.id)).then(() => client.deleteChat(chat.id))
+            } else {
+                client.sendText(chat.groupMetadata.id, `Halo warga grup *${chat.contact.name}* terimakasih sudah menginvite bot ini, untuk melihat menu silahkan kirim *!help*`)
+            }
+        }))
+
+        /*client.onAck((x => {
+            const { from, to, ack } = x
+            if (x !== 3) client.sendSeen(to)
+        }))*/
+
+        // listening on Incoming Call
+        client.onIncomingCall(( async (call) => {
+            await client.sendText(call.peerJid, 'Maaf, saya tidak bisa menerima panggilan. nelfon = block!')
+            .then(() => client.contactBlock(call.peerJid))
+        }))
+    }
+
+create(options(true, start))
+    .then(client => start(client))
+    .catch((error) => console.log(error))
+
 //A187ID
 const
 {
@@ -1267,18 +1320,6 @@ if (text.includes("!alay")){
 		conn.sendMessage(id, hasil, MessageType.text)
 	})
 }
-const fetch = require('node-fetch');
-
-const getBase64 = async (url) => {
-    const response = await fetch(url, { headers: { 'User-Agent': 'okhttp/4.5.0' } });
-    if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
-    const buffer = await response.buffer();
-    const videoBase64 = `data:${response.headers.get('content-type')};base64,` + buffer.toString('base64');
-    if (buffer)
-        return videoBase64;
-};
-
-exports.getBase64 = getBase64;
 
 //Tolonglah bro jangan di ubah ubah Javzlyn Fransiskus Manik
 
