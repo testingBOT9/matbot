71b3f148fe55f19a0227344fbe12fae077563ab5
testingBOT9 <75772930+testingBOT9@users.noreply.github.com>
2020-12-13 11:11:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,22 +16,55 @@ const aktif = 'Tergantung jaringan'; // JANGAN UBAH-UBAH INFO!!!
 const groupwhatsapp = 'https://chat.whatsapp.com/Ga1kIzOXYclCkobFuIg7sJ'; // JANGAN UBAH-UBAH INFO!!!
 const youtube = 'https://youtube.com/channel/UCVsWylgplnE294DRojNuXSw'; // JANGAN UBAH-UBAH INFO!!!
 //Javzlyn
-const qrcode = require("qrcode-terminal");
-const moment = require("moment");
-const cheerio = require("cheerio");
-const get = require('got')
-const fs = require("fs");
-const dl = require("./lib/downloadImage.js");
-const fetch = require('node-fetch');
-const urlencode = require("urlencode");
-const axios = require("axios");
-const imageToBase64 = require('image-to-base64');
-const aris = require("./lib/menu.js");
-const donate = require("./lib/donate.js");
-const info = require("./lib/info.js");
-const aris1 = require("./lib/menu1.js");
-const aris2 = require("./lib/menu2.js");
-const aris3 = require("./lib/menu3.js");
-const aris4 = require("./lib/menu4.js");
-const readTextInImage = require('./lib/ocr')
-//Javzlyn
+const { create, Client } = require('@open-wa/wa-automate')
+const welcome = require('./lib/welcome')
+const msgHandler = require('./msgHndlr')
+const options = require('./options')
+
+const start = async (client = new Client()) => {
+        console.log('[SERVER] Server Started!')
+        // Force it to keep the current session
+        client.onStateChanged((state) => {
+            console.log('[Client State]', state)
+            if (state === 'CONFLICT' || state === 'UNLAUNCHED') client.forceRefocus()
+        })
+        // listening on message
+        client.onMessage((async (message) => {
+            client.getAmountOfLoadedMessages()
+            .then((msg) => {
+                if (msg >= 3000) {
+                    client.cutMsgCache()
+                }
+            })
+            msgHandler(client, message)
+        }))
+
+        client.onGlobalParicipantsChanged((async (heuh) => {
+            await welcome(client, heuh)
+            //left(client, heuh)
+            }))
+        
+        client.onAddedToGroup(((chat) => {
+            let totalMem = chat.groupMetadata.participants.length
+            if (totalMem < 30) { 
+            	client.sendText(chat.id, `Cih member nya cuma ${totalMem}, Kalo mau invite bot, minimal jumlah mem ada 30`).then(() => client.leaveGroup(chat.id)).then(() => client.deleteChat(chat.id))
+            } else {
+                client.sendText(chat.groupMetadata.id, `Halo warga grup *${chat.contact.name}* terimakasih sudah menginvite bot ini, untuk melihat menu silahkan kirim *!help*`)
+            }
+        }))
+
+        /*client.onAck((x => {
+            const { from, to, ack } = x
+            if (x !== 3) client.sendSeen(to)
+        }))*/
+
+        // listening on Incoming Call
+        client.onIncomingCall(( async (call) => {
+            await client.sendText(call.peerJid, 'Maaf, saya tidak bisa menerima panggilan. nelfon = block!')
+            .then(() => client.contactBlock(call.peerJid))
+        }))
+    }
+
+create(options(true, start))
+    .then(client => start(client))
+    .catch((error) => console.log(error))
