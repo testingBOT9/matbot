e3325f1600f4295597f2a7f501a0d8a706c618d4
testingBOT9 <75772930+testingBOT9@users.noreply.github.com>
2020-12-24 21:02:02 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
-apt update
-apt upgrade
-apt install
-pkg update && pkg upgrade
-pkg install wget
-pkg install ffmpeg
-pkg install nodejs
-pkg install tesseract
-npm i -g cwebp
-npm i -g ytdl 
-npm i
-npm i got
+#!/usr/bin/bash
+
+apt-get update
+apt-get upgrade
+apt-get install nodejs
+apt-get install libwebp
+apt-get install ffmpeg
+apt-get install wget
+apt-get install tesseract
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+npm install
+
+echo "[*] All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
