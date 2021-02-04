405ef124f88cac3d110ea00497fc71cbbc33573f
フェブリアン <75772930+testingBOT9@users.noreply.github.com>
2021-02-04 16:42:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const loli = new lolis()
 const welkom = JSON.parse(fs.readFileSync('./src/welkom.json'))
 const nsfw = JSON.parse(fs.readFileSync('./src/nsfw.json'))
 const samih = JSON.parse(fs.readFileSync('./src/simi.json'))
-prefix = '!'
+prefix = '.'
 blocked = []
 
 function kyun(seconds){
